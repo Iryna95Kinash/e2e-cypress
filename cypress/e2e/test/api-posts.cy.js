1d5ea3b172bbb2d7fc53ea9b674132c324b325ed
IrynaKinash <irynakinash@gmail.com>
2023-03-05 17:01:34 +0200

    homework2 GET, POST, PATCH, DELETE

--- cypress/e2e/test/api-posts.cy.js
+++ cypress/e2e/test/api-posts.cy.js
@@ -1,16 +1,19 @@
 const apiPosts = `http://localhost:4200/api/posts`;
 
+// $ yarn add @faker-js/faker --dev
+// $ yarn add @types/faker -D
+
 describe('TODO App', () => {
   context('GET /api/posts', () => {
-      it('gets a list of posts', () => {
-        cy.request('GET', apiPosts).then((response) => {
-          expect(response.status).to.eq(200);
-          expect(response.body.length).to.eq(100);
-        });
+    it('gets a list of posts', () => {
+      cy.request('GET', apiPosts).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.length).to.eq(100);
       });
     });
+  });
 
-    context('GET /api/posts/:postId', () => {
+  context('GET /api/posts/:postId', () => {
     it('get a post', () => {
       const postId = 2;
       cy.request('GET', `http://localhost:4200/api/posts/${postId}`).then((response) => {
@@ -24,8 +27,45 @@ describe('TODO App', () => {
         });
       });
     });
+  });
 
+  context('POST /posts', () => {
+      it('creates a new post', () => {
+        const newPost = {
+//          cy: request('POST', `http://localhost:4200/api/posts/${postId}`).then((response) => {
+//            expect(response.status).to.eq(200);
+//            expect(response.body).to.deep.equal({
+              userId: 1,
+              title: 'New title',
+              body: 'somebody that i used to know',
+//            });
+//          }),
+        };
+      });
+    });
 
+  context('PATCH /posts', () => {
+    it('edit a post', () => {
+      const newPost = {
+        userId: 1,
+        title: 'Updated title',
+        body: 'Updated description',
+      };
+    });
+  });
+
+  context('DELETE /posts', () => {
+      it('delete a post', () => {
+      });
+  });
+
+  context('GET /api/posts/1/comments', () => {
+     it('get comments', () => {
+        cy.request('GET', `http://localhost:4200/api/posts/1/comments`).then((response) => {
+          expect(response.status).to.eq(200);
+          console.log(response);
+        });
+     });
   });
 
 });
